dbe290e6ea921af41917454317b1e8bc07b210b3
dilarak0477 <dilarak0477@gmail.com>
2025-11-07 02:08:35 +0300

    yapıldı (gelen işin adı)

---
+++ src/Gun01/Merhaba.java
@@ -0,0 +1,15 @@
+package Gun01;
+
+public class Merhaba {
+
+    public static void main(String[] args) {
+
+        System.out.println("merhaba 1");
+
+        System.out.println("merhaba 2");
+
+        System.out.println("yapıldı");
+        System.out.println("yapıldı 2");
+    }
+
+}
